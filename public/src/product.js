dbb5d93d2fa91f945a3787fb588c47a5a184da20
maxlestage <maxlestage@icloud.com>
2021-06-29 15:42:27 +0200

    Ajout d'un dossier public comprenant les images et le code js

--- public/src/product.js
+++ public/src/product.js
@@ -6,8 +6,7 @@ fetch("http://localhost:3000/api/cameras")
     let titleNameProduct = document.getElementById("title_Product");
     const productIdUrl = window.location.search;
     // console.log(productIdUrl);
-    // Utilisation de table Regex pour reformaté l'id afin de le verifier avec la liste
-    // Dans ce cas ci nous supprimons "?id=" = .substr(4)
+    // Utilisation de table Regex pour reformaté l'id afin de le verifier avec la liste, dans ce cas ci nous supprimons "?id=" = .substr(4)
     let compactUrl = /[^.]+/.exec(productIdUrl)[0].substr(4);
     // console.log(compactUrl);
     for (let product of data) {
