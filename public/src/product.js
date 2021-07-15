b4fd6aa4d006c6a5ffa06f52c2d9eb5ae42691e6
maxlestage <maxlestage@icloud.com>
2021-07-15 11:20:52 +0200

    test panier localStorage

--- public/src/product.js
+++ public/src/product.js
@@ -9,7 +9,7 @@ fetch(`http://localhost:3000/api/cameras/${compactUrl}`)
     let card = document.getElementById("main");
     let titleCardSpec = document.getElementById("title");
     let titleNameProduct = document.getElementById("title_Product");
-    console.log(data);
+    // console.log(data);
     let lenses = data.lenses;
     // console.log(compactUrl);
     //for (let product of data) {
@@ -30,8 +30,8 @@ fetch(`http://localhost:3000/api/cameras/${compactUrl}`)
     
   </div>
   <div class="form-group mb-2">
-    <label for="staticEmail2" class="sr-only">Email</label>
-    <input type="number" id="quantity" name="quantity" min="1" max="5" class="form-control" placeholder="Quantité :">
+    <label for="staticEmail2" class="sr-only">Selected Quantity</label>
+    <input type="number" id="quantity" name="quantity" min="0" max="5" class="form-control" placeholder="Quantité :">
   </div>
     <button type="button" id="" class="btn btn-primary">Ajouter au panier</button>
   </div>
@@ -54,25 +54,46 @@ fetch(`http://localhost:3000/api/cameras/${compactUrl}`)
       selectLenses.innerHTML += `<option onchange="myFunction()" class="LenseModel" value="${lenses[i]}">${lenses[i]}</option>`;
     }
 
+    // let inputChoice = [];
+    let inputToObject = {};
+    let inputChoice = [
+      { "Hirsch 400DTS": ["60mm 2.8", 5] },
+      { "Zurss 50S": ["35mm 1.4", 2] },
+      { "Zurss 80S": ["35mm 1.4", 2] },
+    ];
+
+    console.log(inputChoice[0]);
     // Get item lense selected
     let itemSelected = (document.getElementById("selectedLense").onchange =
       function () {
-        let value = document.getElementById("selectedLense").value;
-        console.log(value);
+        let valueSelected = document.getElementById("selectedLense").value;
+        console.log(valueSelected);
       });
+
+    // LocalStorage de la valeur
+    var inputItem = document.getElementById("selectedLense");
+    // inputItem.value = localStorage.getItem("Panier");
+    inputItem.oninput = function (event) {
+      localStorage.setItem("Panier", JSON.stringify("test"));
+    };
 
     // Get item quantity
     let itemQuantity = (document.getElementById("quantity").onchange =
       function () {
-        let value = document.getElementById("quantity").value;
-        console.log(value);
+        let valueQuantity = document.getElementById("quantity").value;
+        console.log(valueQuantity);
       });
+    // LocalStorage de la quantity
+    var inputQuantity = document.getElementById("quantity");
+    // inputQuantity.value = localStorage.getItem("panier");
+    inputQuantity.oninput = function (event) {
+      localStorage.setItem("panier", inputQuantity.value);
+    };
 
-    // Je crée une variable qui va stocker mes choix dans le local storage
-    let myChoiceSelection = localStorage.setItem(
-      itemSelected,
-      document.querySelector("option").value
-    );
+    //     Si je découpe le boulot :
+    // 1 je redéfini la clé Key sur "Panier", sa value est un tableau[] vide
+    // 2 je dois ajouter dans objet la selection des éléments {}
+    // 3 Je dois push cet objet dans ma value []
 
     // je crée une variable qui au click du bouton utilisera les deux variable précedentes
   });
--- public/src/purchase.js
+++ public/src/purchase.js
@@ -1,9 +1,37 @@
-const productIdUrl = window.location.search;
-// console.log(productIdUrl);
-// Utilisation de table Regex pour reformaté l'id afin de le verifier avec la liste, dans ce cas ci nous supprimons "?id=" = .substr(4)
-let compactUrl = /[^.]+/.exec(productIdUrl)[0].substr(4);
-fetch(`http://localhost:3000/api/cameras/${compactUrl}`)
+fetch("http://localhost:3000/api/cameras")
   .then((response) => response.json())
   .then((data) => {
+    let productName = data.name;
     console.log(data);
+
+    // let inputChoice = [
+    //   { "Hirsch 400DTS": ["60mm 2.8", 5] },
+    //   { "Zurss 50S": ["35mm 1.4", 2] },
+    //   { "Zurss 80S": ["35mm 1.4", 2] },
+    // ];
+
+    const inputChoice = {
+      a: "somestring",
+      b: 42,
+    };
+
+    for (const [key, value] of Object.entries(object1)) {
+      console.log(`${key}: ${value}`);
+    }
+
+    localStorage.setItem = ("Panier", JSON.stringify(inputChoice[0][1]));
+    console.log(
+      (localStorage.setItem = ("Panier", JSON.stringify(inputChoice[0][1])))
+    );
+    var output = document.createElement("p");
+    output.textContent = localStorage.getItem("Panier");
+
+    console.log(localStorage.getItem("Panier"));
+    console.log(output.textContent);
+    // window.addEventListener("storage", function (event) {
+    //   if (event.key === "Panier") {
+    //     output.textContent = event.newValue;
+    //   }
+    // });
+    document.body.appendChild(output);
   });
